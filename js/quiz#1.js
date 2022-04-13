020e31debc1fcbfd2ca0dc82978dda1fcdd62648
TaylorMade <warriorotk18@gmail.com>
2022-04-13 17:02:24 -0600

    Completed array

---
+++ js/quiz#1.js
@@ -0,0 +1,6 @@
+const psQuiz = [
+    ['What is your favorite PS1 game?', 'RE2'],
+    ['What is your first PS1 game you played?', 'Crash Bandicoot'],
+    ['What is your favorite FF on PS1?', '9'],
+    ['How old were you when you got a PS1?', '8']
+]
