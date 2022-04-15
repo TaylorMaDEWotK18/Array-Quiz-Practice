1044da5721e87ec23b314f0f8b5b4743ffdb762c
TaylorMade <warriorotk18@gmail.com>
2022-04-15 16:27:09 -0600

    Trying an ex to interpolate an array in an array

--- js/quiz2.js
+++ js/quiz2.js
@@ -1,7 +1,9 @@
 const head = `What are some Stephen King Books you are looking for!?`;
 document.querySelector('h1').innerHTML = head;
 
-const inStockKing = []
+const darkTower = ['The Gunslinger', 'The Drawing of the Three', 'The Wastelands', 'Wizard and Glass', 'Wolves of the Calla', 'Song of Susannah', 'The Dark Tower']
+
+const inStockKing = ['Carrie', 'Skeleton Crew', 'The Shining', 'Cujo', 'The Gunslinger', '', '', '']
 // So the goal here is to search whether the system is in stock and have the page display the answer and what we searched for. We'll use the 'includes' method and a conditional statement
 
 // Changing up the original if cond. and a making it an 'if else' cond and a Logical NOT Operator so that if null is entered it wil display specific message
