8cd7ff18fcbb1e7b0383bf03055abfe916377a08
TaylorMade <warriorotk18@gmail.com>
2022-04-15 16:22:52 -0600

    Set up new challenge

---
+++ js/quiz2.js
@@ -0,0 +1,8 @@
+const head = `What are some Stephen King Books you are looking for!?`;
+document.querySelector('h1').innerHTML = head;
+
+const inStockKing = []
+// So the goal here is to search whether the system is in stock and have the page display the answer and what we searched for. We'll use the 'includes' method and a conditional statement
+
+// Changing up the original if cond. and a making it an 'if else' cond and a Logical NOT Operator so that if null is entered it wil display specific message
+
