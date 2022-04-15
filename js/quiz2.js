1b719723568986e300b2e1b5849bea93ee0350a3
TaylorMade <warriorotk18@gmail.com>
2022-04-15 16:29:26 -0600

    quick console check on the interpolated array

--- js/quiz2.js
+++ js/quiz2.js
@@ -3,8 +3,9 @@ document.querySelector('h1').innerHTML = head;
 
 const darkTower = ['The Gunslinger', 'The Drawing of the Three', 'The Wastelands', 'Wizard and Glass', 'Wolves of the Calla', 'Song of Susannah', 'The Dark Tower']
 
-const inStockKing = ['Carrie', 'Skeleton Crew', 'The Shining', 'Cujo', 'The Gunslinger', '', '', '']
+const inStockKing = ['Carrie', 'Skeleton Crew', 'The Shining', 'Cujo', `${darkTower}`, 'It', 'The Stand', 'The Raft']
 // So the goal here is to search whether the system is in stock and have the page display the answer and what we searched for. We'll use the 'includes' method and a conditional statement
 
 // Changing up the original if cond. and a making it an 'if else' cond and a Logical NOT Operator so that if null is entered it wil display specific message
 
+console.log(inStockKing);
