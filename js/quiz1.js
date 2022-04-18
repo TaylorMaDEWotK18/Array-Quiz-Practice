9ed655c99050355e946e788ca34c3be66ce30f35
TaylorMade <warriorotk18@gmail.com>
2022-04-17 23:55:39 -0600

    Set variables and started forLoop, testing

--- js/quiz1.js
+++ js/quiz1.js
@@ -1,47 +1,47 @@
-const psQuiz = [
-    ['What is your favorite PS1 game?', 'RE2'],
-    ['What is your first PS1 game you played?', 'Crash Bandicoot'],
-    ['What is your favorite FF on PS1?', '9'],
-    ['How old were you when you got a PS1?', '8']
-]
+// const psQuiz = [
+//     ['What is your favorite PS1 game?', 'RE2'],
+//     ['What is your first PS1 game you played?', 'Crash Bandicoot'],
+//     ['What is your favorite FF on PS1?', '9'],
+//     ['How old were you when you got a PS1?', '8']
+// ]
 
-let correct = [];
-let incorrect = [];
-let correctAnswers = 0;
+// let correct = [];
+// let incorrect = [];
+// let correctAnswers = 0;
 
-for ( let x = 0; x < psQuiz.length; x++ ) {
-    let questions = psQuiz[x][0];
-    let answers = psQuiz[x][1];
-    let response = prompt(questions);
+// for ( let x = 0; x < psQuiz.length; x++ ) {
+//     let questions = psQuiz[x][0];
+//     let answers = psQuiz[x][1];
+//     let response = prompt(questions);
 
-    if ( response === answers) {
-        correctAnswers++; 
-        correct.push(questions);
-    } else {
-        incorrect.push(questions);
-    }
-}
+//     if ( response === answers) {
+//         correctAnswers++; 
+//         correct.push(questions);
+//     } else {
+//         incorrect.push(questions);
+//     }
+// }
 
-function testScore(arr) {
-    items = ``;
-    for( let x = 0; x < arr.length; x++ ) {
-        items += `<li>${arr[x]}</li>`;
-    }
-    return items;
-}
+// function testScore(arr) {
+//     items = ``;
+//     for( let x = 0; x < arr.length; x++ ) {
+//         items += `<li>${arr[x]}</li>`;
+//     }
+//     return items;
+// }
 
-let result = `<h1>You got a total of ${correctAnswers} questions correct.</h1>`;
+// let result = `<h1>You got a total of ${correctAnswers} questions correct.</h1>`;
 
-document.querySelector('main').innerHTML = `${result}
-    <h2>Questions you got right!</h2>
-    <ol>
-        ${testScore(correct)}
-    </ol>
-    <h2>INCORRECT Questions!</h2>
-    <ol>
-        ${testScore(incorrect)}
-    </ol>
-`;
+// document.querySelector('main').innerHTML = `${result}
+//     <h2>Questions you got right!</h2>
+//     <ol>
+//         ${testScore(correct)}
+//     </ol>
+//     <h2>INCORRECT Questions!</h2>
+//     <ol>
+//         ${testScore(incorrect)}
+//     </ol>
+// `;
 
 // Practice run #2
 
@@ -51,3 +51,11 @@ const reAsks = [
     ['What Nemesis remake very good?', 'It was, but too short!'],
     ['Favorite enemy from RE4?', 'Lake Monster']
 ]
+
+let correct = [];
+let incorrect = [];
+let correctAnswers = 0;
+
+for( let x = 0; x < reAsks.length; x++) {
+    let questions = prompt(`${reAsks[x][0]}`);
+}
