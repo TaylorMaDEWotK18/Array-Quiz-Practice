3346e9f78fa6e1fede41d52d582535efec8331ad
TaylorMade <warriorotk18@gmail.com>
2022-04-19 00:24:48 -0600

    Loaded some variable

--- js/quiz3.js
+++ js/quiz3.js
@@ -1,3 +1,6 @@
 //Through this practice session I want to enter a specific element from the array and have it display a specific message for that selection
 
 const fondMemGames = ['Star Fox', 'Super Mario 3', 'RE2', 'The Bouncer', 'Final Fantasy 9', 'Gears of War']
+
+let message;
+const request = prompt('What is a game that brings a lot of nostalgia to you from your childhood?');
