14443c7b766f50e512bf3eb01fddb907772f8518
TaylorMade <warriorotk18@gmail.com>
2022-04-19 00:22:27 -0600

    Created array for 3rd Quiz

---
+++ js/quiz3.js
@@ -0,0 +1,3 @@
+//Through this practice session I want to enter a specific element from the array and have it display a specific message for that selection
+
+const fondMemGames = ['Star Fox', 'Super Mario 3', 'RE2', 'The Bouncer', 'Final Fantasy 9', 'Gears of War']
