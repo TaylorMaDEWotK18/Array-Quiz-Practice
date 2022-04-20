cd5719b206475373923649e1a42308b9090779b6
TaylorMade <warriorotk18@gmail.com>
2022-04-19 18:47:47 -0600

    Think everything is good to merge now

---
+++ js/quiz3.js
@@ -0,0 +1,16 @@
+//Through this practice session I want to enter a specific element from the array and have it display a specific message for that selection
+
+const fondMemGames = ['Star Fox', 'Super Mario 3', 'RE2', 'The Bouncer', 'Final Fantasy 9', 'Gears of War']
+
+let message;
+const request = prompt('What is a game that brings a lot of nostalgia to you from your childhood?');
+
+if (request === fondMemGames[2]) {
+    message = `Resident Evil has to be one of the greatest series of all time! And RE2 takes GOLD!`;
+} else if(fondMemGames.includes(request)) {
+    message = `Man ${request} was amazing! Great game!`;
+} else {
+    message = `Nope, ${request} wasn't cool enough!`;
+}
+
+document.querySelector('h1').innerHTML = message;
