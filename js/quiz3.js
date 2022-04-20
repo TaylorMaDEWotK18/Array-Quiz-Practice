69c5896f189b845ab8773599195e5529943bf546
TaylorMade <warriorotk18@gmail.com>
2022-04-20 17:00:20 -0600

    Starting new practice sesh, tailored up the array

--- js/quiz3.js
+++ js/quiz3.js
@@ -1,18 +1,22 @@
-//Through this practice session I want to enter a specific element from the array and have it display a specific message for that selection
+// //Through this practice session I want to enter a specific element from the array and have it display a specific message for that selection
 
-const fondMemGames = ['Star Fox', 'Super Mario 3', 'RE2', 'The Bouncer', 'Final Fantasy 9', 'Gears of War']
+// const fondMemGames = ['Star Fox', 'Super Mario 3', 'RE2', 'The Bouncer', 'Final Fantasy 9', 'Gears of War']
 
-let message;
-const request = prompt('What is a game that brings a lot of nostalgia to you from your childhood?');
+// let message;
+// const request = prompt('What is a game that brings a lot of nostalgia to you from your childhood?');
 
-if (request === fondMemGames[2]) {
-    message = `Resident Evil has to be one of the greatest series of all time! And RE2 takes GOLD!`;
-} else if (request === fondMemGames[3]) {
-    message = `Wow... How amazing was ${request}.. The first time bootin up that PS2, playing for hours and hours.. Amazing`
-} else if (fondMemGames.includes(request)) {
-    message = `Man ${request} was amazing! Great game!`;
-} else {
-    message = `Nope, ${request} wasn't cool enough!`;
-}
+// if (request === fondMemGames[2]) {
+//     message = `Resident Evil has to be one of the greatest series of all time! And RE2 takes GOLD!`;
+// } else if (request === fondMemGames[3]) {
+//     message = `Wow... How amazing was ${request}.. The first time bootin up that PS2, playing for hours and hours.. Amazing`
+// } else if (fondMemGames.includes(request)) {
+//     message = `Man ${request} was amazing! Great game!`;
+// } else {
+//     message = `Nope, ${request} wasn't cool enough!`;
+// }
 
-document.querySelector('h1').innerHTML = message;
+// document.querySelector('h1').innerHTML = message;
+
+// Next attempt I'm going to build similar program only using a for loop to cycle through
+
+const fondMemGames = ['Star Fox', 'Super Mario World', 'Super Mario 3', 'RE2', 'Star Craft', 'Half-Life', 'Final Fantasy 9', 'Gears of War']
