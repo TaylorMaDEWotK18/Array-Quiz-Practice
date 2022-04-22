3e1de110a7792e0634905e779f7d57210b9ed4f3
TaylorMade <warriorotk18@gmail.com>
2022-04-22 00:26:44 -0600

    starting array complete and beginning function

--- js/quiz4.js
+++ js/quiz4.js
@@ -1,1 +1,5 @@
 // In this challenge I want to create a list and everytime I enter something into the prompt, it's added to the list. Lets go!
+
+const topTitles = [ 'Super Smash', 'Resident Evil 4', 'Legend of Zelda: Ocarina of Time']
+
+function listTitles()
